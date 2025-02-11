113063188454a6e183b1db2780fc3bc2f7d36b7f
Arctusol <73721443+Arctusol@users.noreply.github.com>
2025-02-11 11:15:15 +0100

    Mise à jour du fichier .gitignore pour ignorer les fichiers JSON de compte et ajout d'URLs de production dans la configuration

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -19,7 +19,9 @@ class Settings(BaseSettings):
         "http://localhost:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5173",
-        "http://127.0.0.1:5174"
+        "http://127.0.0.1:5174",
+        "https://tracking-budget-cti1yf10f-arctusols-projects.vercel.app",
+        "https://tracking-budget-ten.vercel.app"
     ]
     
     # Security
